1adfac4c076e97487318a7b34f154ab3c4a0708f
JBicera <joshuaebicera@gmail.com>
2023-11-15 00:06:33 -0800

    Finished

--- CS 3560/a2/Admin.java
+++ CS 3560/a2/Admin.java
@@ -6,6 +6,8 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -16,10 +18,23 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 
-public class Admin {
+//Subject(Observer) interface
+interface Subject {
+    void registerObserver(Observer observer);
+    void notifyObservers(String tweet);
+}
+//Visitor Interface
+interface UserVisitor
+{
+	void visit(User user);
+}
+
+
+public class Admin implements Subject, UserVisitor {
 
 	private static Admin instance;  // Singleton instance
 	
+
 	//Instance Variables
 	private DefaultMutableTreeNode treeRoot;
 	private JTextArea userID;
@@ -29,6 +44,7 @@ public class Admin {
 	private User[] userObjects;
 	private int numUsers;
 	private int numGroups;
+	private List<Observer> observers;
 
 	//Create buttons
 	private JButton addUser;
@@ -42,48 +58,102 @@ public class Admin {
 	//Display data in a hiearchal tree
 	private JTree tree;
 
-	//Map to store followers for each user
-
-	//Method to get the Singleton instance
+	//Method to get the single instance of Admin
     public static Admin getInstance() {
         if (instance == null) {
             instance = new Admin();
-            // Additional setup if needed
         }
         return instance;
     }
-	//Notify followers about the new tweet
-    public void notifyFollowers(String userID, String tweet, String[] followers) {
-        // Update the news feed of each follower
-		for (int i = 0; i < followers.length; i++) {
-                if (followers[i].equals(userID)) {
-                        userObjects[i].getTweetModel().addElement(tweet);
-                        userObjects[i].getNewsFeed().repaint();
-                        break;
-                    }
-            }
+	//Observer pattern methods
+    @Override
+	//Adds new users into the list of observers.
+	//This method follows to the Observer pattern by allowing users to register themselves
+	//as observers. When a new user is created or added to the system, they register as an
+	//observer to receive updates about tweets.
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
     }
-	//Private constructor to enforce Singleton pattern
+
+	//No need to remove observers as there are no need to remove users
+
+    @Override
+	//This method follows the Observer pattern by iterating through the list of registered
+	//observers and notifying each of them about the new tweet. Each observer (user) is
+	//expected to implement the update method to handle the received tweet.
+    public void notifyObservers(String tweet) {
+        for (Observer observer : observers) {
+            observer.update(tweet);
+        }
+    }
+
+	//Visitor Pattern method
+	@Override
+	//When the program needs to visit a user, it opens up their individual GUI.
+	//This method adheres to the Visitor pattern, where the Admin (visitor) can visit
+	//individual User objects. In this case, when the Admin visits a User, it triggers
+	//the opening of the User's individual GUI, allowing the Admin to interact with
+	//and view details specific to that User.
+	public void visit(User user)
+	{
+		user.setVisible();
+	}
+
+
+	//Update following in the admin
+    public void updateFollowing(String currUser, String followingUser) {
+        for (int i = 0; i < userObjects.length; i++) {
+			//Find the user being followed
+            if (userObjects[i] != null && userObjects[i].getUsername().equals(followingUser)) {
+                userObjects[i].addFollower(currUser); //Add the current user to their following
+                break;
+            }
+        }
+    }
+	//Notify followers about the new tweet
+	public void notifyFollowers(String tweet, String[] followers) {
+		// Update the news feed of each follower
+		for (String follower : followers) {
+			for (User user : userObjects) {
+                // Iterate through all Users and find the follower
+                if (user != null && user.getUsername().equals(follower)) {
+                    user.update(tweet); // Notify followers
+					break;
+                }
+            }
+		}
+	}
+	//Singleton Pattern
+	//The private constructor ensures that only one instance of the Admin class can be created.
+	//This is a key aspect of the Singleton pattern, where a class has only one instance, and
+	//provides a global point of access to that instance. This is important for the program
+	//as there is only one admin that controls and holds all other users and is the root at which
+	//all user interaction takes place
 	private Admin()
 	{
 		//Initialize instance variabels
-		users = new String[100];
-		groups = new String[100];
-		userObjects = new User[100];
+		users = new String[10];
+		groups = new String[10];
+		userObjects = new User[10];
 		numUsers = 0;
 		numGroups = 0;
+		 observers = new ArrayList<>();
 	}
+
+	//Method to setup and deploy the GUI
 	private void setupGUI(){
 		//Sets JFrame
 		JFrame jfrm = new JFrame("Admin");
-		jfrm.setSize(800,400);
+		jfrm.setSize(600,350);
 		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		Border simpleBorder = BorderFactory.createLineBorder(Color.BLACK);
 		//Create main panel
+		Border simpleBorder = BorderFactory.createLineBorder(Color.DARK_GRAY);
 		JPanel mainPanel = new JPanel();
-		mainPanel.setBorder(new TitledBorder("Admin"));
-		mainPanel.setLayout(new GridLayout(1,2,7,7)	);
+		mainPanel.setBorder(new TitledBorder("Mini Twitter"));
+		mainPanel.setBackground(Color.LIGHT_GRAY);
+		mainPanel.setLayout(new GridLayout(1,2,7,7));
+
 		//Create tree panel
 		JPanel treePanel = new JPanel();
 		treePanel.setBorder(new TitledBorder("Tree View"));
@@ -91,6 +161,7 @@ public class Admin {
 		tree = new JTree(treeRoot);
 		treePanel.setBorder(simpleBorder);
 		treePanel.add(tree);
+
 		//Create config panel
 		JPanel configPanel = new JPanel();
 		configPanel.setLayout(new BorderLayout());
@@ -107,11 +178,13 @@ public class Admin {
 		treeConfig.add(groupId);
 		treeConfig.add(addGroup);
 		
+		//User view for individual users
 		JPanel userView = new JPanel();
 		userView.setLayout(new BorderLayout());
 		openUserView = new JButton("Open User View");
 		userView.add(openUserView, BorderLayout.NORTH);
 		
+		//Buttons for stats of the mini-twitter
 		JPanel showStats = new JPanel();
 		showStats.setLayout(new GridLayout(2,2,5,5));
 		showUserTotal = new JButton("Show User Total");
@@ -122,107 +195,116 @@ public class Admin {
 		showStats.add(showMessagesTotal);
 		showStats.add(showGroupTotal);
 		showStats.add(showPositivePercentage);
-		
+
 		configPanel.add(treeConfig, BorderLayout.NORTH);
 		configPanel.add(userView);
 		configPanel.add(showStats, BorderLayout.SOUTH);
-		
 		mainPanel.add(treePanel, BorderLayout.WEST);
-		mainPanel.add(configPanel, BorderLayout.EAST);
-		
+		mainPanel.add(configPanel, BorderLayout.EAST);	
+
+
+		//Open user's view button
+		openUserView.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode)tree.getLastSelectedPathComponent();
+				if(selectedNode != null) {
+					//Find User Object and then open it
+					for(int i = 0; i < numUsers; i++) {
+						if(users[i] != null && users[i].equals(selectedNode.toString()))
+							visit(userObjects[i]);
+					}
+				}
+				return;
+			}
+		});
+
+		//Add User Button
 		addUser.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
 				if (selectedNode == null) {
 	                    selectedNode = treeRoot;
 	            }
-				
-				// Check for duplicate users
+				//Check if the user alread exists
 				for (int i = 0; i < numUsers; i++) {
 					if (users[i] != null && users[i].equals(userID.getText())) {
 						JOptionPane.showMessageDialog(null, "User ID already exists. Please choose a different ID.");
 						return;
 					}
 				}
-				
+
+				//Add onto the tree
 				DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(userID.getText());
 				selectedNode.add(newNode);
-				
-				
-				userObjects[numUsers] = new User(userID.getText(), Admin.this);//Pass same Admin instance to User
+
+				//Create a new user
+				userObjects[numUsers] = new User(userID.getText(), Admin.getInstance());//Pass same Admin instance to User
 				users[numUsers] = userID.getText();
+				//Register as observer
+				Admin.getInstance().registerObserver(userObjects[numUsers]);
 				numUsers += 1;
 
+				//Update tree
 				DefaultTreeModel model = (DefaultTreeModel)tree.getModel();
 	            model.nodeStructureChanged(selectedNode);
-	            
 	            TreePath path = new TreePath(newNode.getPath());
                 tree.expandPath(path);
-                
                 tree.setSelectionPath(null);
 			}
 		});
-		
+
+		//Add Group Button
 		addGroup.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				DefaultMutableTreeNode selectedNode = treeRoot;
-				
 				for (int x = 0; x < numGroups; x++) {
 		            if (groups[x].equals(groupId.getText())) {
 		                return;
 		            }
 		        }
-				
+				//Add Group node
 				DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(groupId.getText());
 				selectedNode.add(newNode);
-				
+
+				//Update counters
 				groups[numGroups] = groupId.getText();
 				numGroups += 1;
-				
+
+				//Update Tree
 				DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
 	            model.nodeStructureChanged(selectedNode);
-	            
 	            TreePath path = new TreePath(newNode.getPath());
                 tree.expandPath(path);
-                
                 tree.setSelectionPath(null);
 			}
 		});
 		
+		//Show number of users button
 		showUserTotal.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
-				System.out.print("Number of users: " + numUsers + "\n");
+				JOptionPane.showMessageDialog(null, "Number of users: " + numUsers);
 			}
 		});
 		
+		//Show number of groups button
 		showGroupTotal.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
-				System.out.print("Number of groups: " + numGroups + "\n");
+				JOptionPane.showMessageDialog(null, "Number of groups: " + numGroups);
 			}
 		});
-			
-		openUserView.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
-				DefaultMutableTreeNode selected = (DefaultMutableTreeNode)tree.getLastSelectedPathComponent();
-				if (selected != null) {
-					for(int i = 0; i < numUsers; i++) {
-						if(users[i] != null && users[i].equals(selected.toString()))
-							userObjects[i].setVisible();
-					}
-				}
-				return;
-			}
-		});
+		
+		//Show total number of messages button
 		showMessagesTotal.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				int totalMessages = 0;
 				for (int i = 0; i < numUsers; i++) {
-					totalMessages += userObjects[i].getTotalMessages();
+					totalMessages += userObjects[i].getTotalMessages(); //Add all tweets form each user
 				}
 				JOptionPane.showMessageDialog(null, "Total number of Tweet messages: " + totalMessages);
 			}
 		});
 		
+		//Show the ratio/percentage of "good" tweets
 		showPositivePercentage.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				int positiveMessages = 0;
@@ -235,12 +317,18 @@ public class Admin {
 				JOptionPane.showMessageDialog(null, "Positive Percentage: " + percentage + "%");
 			}
 		});
-		
-		
+
+		//Sets up tree display
+		//This method follows the Composite Pattern by treating the tree nodes uniformly,
+		//where both individual users (leaf nodes) and groups (composite nodes) are considered.
+		//The method checks the selected node in the tree, enabling or disabling buttons
+		//based on the type of node selected:
+		// - If no node is selected, both "Add User" and "Add Group" buttons are disabled.
+		// - If the selected node is the root, both "Add User" and "Add Group" buttons are enabled.
+		// - If the selected node is a group, only the "Add User" button is enabled.
 		tree.addTreeSelectionListener(new TreeSelectionListener() {
             public void valueChanged(TreeSelectionEvent e) {
             	DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-
                 if (selectedNode == null) {
                     addUser.setEnabled(false);
                     addGroup.setEnabled(false);
@@ -259,9 +347,7 @@ public class Admin {
                 }
             }
         });
-		
 		jfrm.add(mainPanel);
-		
 		jfrm.setVisible(true);
 	}
 
--- CS 3560/a2/User.java
+++ CS 3560/a2/User.java
@@ -10,33 +10,52 @@ import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import java.lang.String;
 
-public class User{
-	//Private Variables
-	private String currUser;
+//Observer interface
+interface Observer {
+    void update(String tweet);
+}
+//Visitor Interface
+interface VisitorComponent{
+	void accept(UserVisitor visitor);
+}
+//Represents the individual user of the mini-twitter
+public class User implements Observer, VisitorComponent {
+	//JFrame
+	private JFrame jfrm;
+	//Text area to 
 	private JTextArea userID;
+	private JTextArea tweetMessage;
     private JButton followUser;
-    private JTextArea tweetMessage;
-    private JButton tweet;
-    private JList<String> newsFeed;
-    private JFrame jfrm;
-    private DefaultListModel<String> tweetModel;
-    private String[] followers = new String[100];
-    private String[] messages = new String[100];
-    private int totalMessages = 0;
-    private int positiveMessages = 0;
-    private int numFollow = 0;
+	private JButton tweet;
 
-	
+	//Lists the twitter feed
+    private JList<String> newsFeed;
+    private DefaultListModel<String> tweetModel; 
+		
+	//Lists who you are following
+	private DefaultListModel<String> followingModel; 
+	private JList<String> followingList;
+
+	//Private Variables
+	private String currUser; //Name of the user
+    private String[] followers = new String[10];//Who the user is following
+    private String[] messages = new String[10];//All tweets
+    private int totalMessages = 0; //Number of tweets
+    private int positiveMessages = 0; //Number of positive tweets
+    private int numFollowers = 0; //Number of people who follow you
+
+	//Initializes with the user's twitter name and the same admin
 	User(String currUser,final Admin admin){
 		this.currUser = currUser;
 		//Initialize JFrame
 		jfrm = new JFrame("User");
 		jfrm.setSize(300,400);
-		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jfrm.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	
-		//2 Rows, 1 Column
+		//Sets main panel
 		JPanel mainPanel = new JPanel();
 		mainPanel.setLayout(new GridLayout(2,1,7,7));
+		mainPanel.setBackground(Color.LIGHT_GRAY);
 		mainPanel.setBorder(new TitledBorder(currUser));
 
 		//Top and bottom parts of the main panel
@@ -56,87 +75,103 @@ public class User{
 		userBottomSubPanel2.setLayout(new GridLayout(1,1,5,5));
 		
 		//Initialize private variables
+		tweetModel = new DefaultListModel<String>();
+		followingModel = new DefaultListModel<String>();
 		userID = new JTextArea(currUser);
 		followUser = new JButton("Follow User");
 		tweetMessage = new JTextArea("Tweet Message");
 		tweet = new JButton("Post Tweet");
 		newsFeed = new JList<>(tweetModel = new DefaultListModel<>());
-		
-		//Sets black border
-		Border simpleBorder = BorderFactory.createLineBorder(Color.BLACK);
+		followingList = new JList<>(followingModel);
+
+		//Sets Dark Grey border
+		Border simpleBorder = BorderFactory.createLineBorder(Color.DARK_GRAY);
 		userID.setBorder(simpleBorder);
 		tweetMessage.setBorder(simpleBorder);
 		newsFeed.setBorder(simpleBorder);
 		
+		//Add in buttons
 		userTopSubPanel.add(userID);
 		userTopSubPanel.add(followUser);
+		userTopSubPanel2.add(followingList);
 		userBottomSubPanel2.add(newsFeed);
 		userBottomSubPanel.add(tweetMessage);
 		userBottomSubPanel.add(tweet);
 		
+		//Adjust subpanels
 		userTopJPanel.add(userTopSubPanel, BorderLayout.NORTH);
 		userTopJPanel.add(userTopSubPanel2, BorderLayout.CENTER);
 		userBottomJPanel.add(userBottomSubPanel,BorderLayout.NORTH);
 		userBottomJPanel.add(userBottomSubPanel2, BorderLayout.CENTER);
 		
+		//Add in subpanels to main panel
 		mainPanel.add(userTopJPanel);
 		mainPanel.add(userBottomJPanel);
-		
+
+		//Add main panel into the J Frame
 		jfrm.add(mainPanel);
 		
+		//Tweet button
 		tweet.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				String message = tweetMessage.getText();
+				//Check if tweet is positive
 				if(message.toLowerCase().contains("good") ||
 					message.toLowerCase().contains("great") ||
 					message.toLowerCase().contains("excellent"))
 					positiveMessages += 1;
 
+				//Add tweet into feed
 				String userTweet = "- " + currUser + ": " + message;
 				messages[totalMessages] = userTweet;
 				totalMessages += 1;
-				
 				tweetModel.addElement(userTweet);
-				
 				newsFeed.repaint();
-				
-				admin.notifyFollowers(currUser, userTweet, followers);
+				//Notify followers of tweet and add it into their feed
+				admin.notifyFollowers(userTweet, followers);
 			}
 		});
 
+		//Button to follow a user given their twitter name
 		followUser.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
-				followers[numFollow] = userID.getText();
-				numFollow += 1;
+				String followingUser = userID.getText();
+				//Add to the list of following
+				followingModel.addElement("- " + followingUser);
+        		followingList.repaint();
+				//Notify the admin to add the current user into their followers
+				admin.updateFollowing(currUser, followingUser); //Update the user you followed that they now have another follower
 			}
 		});
 	}
-
-	public void setVisible() {
-		jfrm.setVisible(true);
-	}
-	public int getTotalMessages() {
-        return totalMessages;
+	//Observer Functions
+	@Override
+    public void update(String tweet) {
+        // Handle the tweet update
+        getTweetModel().addElement(tweet);
+        getNewsFeed().repaint();
     }
-
-    public int getPositiveMessages() {
-        return positiveMessages;
-    }
-	public String getUsername(){
-		return currUser;
+	@Override
+	public void accept(UserVisitor visitor) {
+		visitor.visit(this);
 	}
-	
-    public double getPositivePercentage() {
+
+	//Helper Functions
+	public void addFollower(String follower) {
+        followers[numFollowers] = follower;
+		numFollowers+=1;
+    }
+	public double getPositivePercentage() {
         if (totalMessages == 0) {
             return 0.0;
         }
         return ((double) positiveMessages / totalMessages) * 100.0;
     }
-	public JList<String> getNewsFeed(){
-		return newsFeed;
-	}
-	public DefaultListModel<String> getTweetModel()
-	{
-		return tweetModel;
-	}
+	//Getter functions
+	public void setVisible() {jfrm.setVisible(true);}
+	public int getTotalMessages() {return totalMessages;}
+    public int getPositiveMessages() {return positiveMessages;}
+	public String getUsername(){return currUser;}
+	public JList<String> getNewsFeed(){return newsFeed;}
+	public DefaultListModel<String> getTweetModel(){return tweetModel;}
 }
